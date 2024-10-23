9a120dd0dcdd0e00429c3dccd68e9e9caa8aca61
Luke <luke7ajb@gmail.com>
2024-10-22 21:33:26 -0400

    Created tag model and factory

---
+++ backend/app/Models/Tag.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+
+class Tag extends Model
+{
+    use HasFactory, HasUuids;
+
+    /**
+     * Indicates if timestamps created_at and updated_at should be created.
+     *
+     * @var bool
+     */
+    public $timestamps = false;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'tag',
+    ];
+
+    /**
+     * Get the attributes that should be cast.
+     *
+     * @return array<string, string>
+     */
+    protected function casts(): array
+    {
+        return [
+            'is_banned' => 'boolean',
+        ];
+    }
+
+    /**
+     * The users that belong to the tag.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
+     */
+    public function users(): BelongsToMany
+    {
+        return $this->belongsToMany(User::class);
+    }
+}
