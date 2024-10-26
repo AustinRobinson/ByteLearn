0e0d3b235dd8fd9f4f52752c8f7a1a6fe73cc822
Luke <luke7ajb@gmail.com>
2024-10-26 19:50:07 -0400

    Created video model

---
+++ backend/app/Models/Video.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Video extends Model
+{
+    use HasUuids;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'user_id',
+        's3_key',
+        'title',
+        'description',
+        'likes',
+        'is_banned',
+    ];
+
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var array<int, string>
+     */
+    protected $hidden = [];
+
+    /**
+     * Get the attributes that should be cast.
+     *
+     * @return array<string, string>
+     */
+    protected function casts(): array
+    {
+        return [
+            'is_banned' => 'boolean',
+        ];
+    }
+
+    /**
+     *  Get the user that owns the video.
+     */
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+}
