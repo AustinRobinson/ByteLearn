779c182d7a4a5b951f3e877dc0d3a366c2798648
austinrobinson <amrobinson28@icloud.com>
2024-11-13 16:55:16 -0500

    add login form functionality

--- frontend/src/pages/login/login.component.ts
+++ frontend/src/pages/login/login.component.ts
@@ -1,14 +1,45 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../../components/header/header.component';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { AuthService } from '../../services/auth/auth.service';
+
+
+export interface LoginFormData {
+  email: string;
+  password: string;
+};
 
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  loginForm = new FormGroup({
+    email: new FormControl(''),
+    password: new FormControl(''),
+  });
+
+  constructor(private authService: AuthService) {
+
+  }
+
+  onSubmit() {
+    // get form values
+    const formValues = this.loginForm.value;
+
+    // authenticate using auth service
+    this.authService.login(formValues as LoginFormData).subscribe({
+      next: (response) => {
+        console.log('Login successful:', response);
+      },
+      error: (error) => {
+        console.error('Login failed:', error.message);
+      }
+    });
+  }
 
 }
--- frontend/src/services/auth/auth.service.ts
+++ frontend/src/services/auth/auth.service.ts
@@ -1,13 +1,31 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { catchError, tap, throwError } from 'rxjs';
+import { LoginFormData } from '../../pages/login/login.component';
+import { environment } from '../../environments/environment';
+
+interface LoginResponse {
+  accessToken: string;
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+  private accessToken: string | null = null;
 
   constructor(private http: HttpClient) {
 
   }
 
+  login(formData: LoginFormData) {
+    return this.http.post<LoginResponse>(`${environment.apiBaseUrl}/login`, formData).pipe(
+      tap((response) => {
+        this.accessToken = response.accessToken;
+      }),
+      catchError((error) => {
+        return throwError(() => new Error('Failed to login'));
+      })
+    );
+  }
 }
