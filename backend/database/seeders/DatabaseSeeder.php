5b866dea979592ef55efb9ffd75922b092acf5d5
Luke <luke7ajb@gmail.com>
2024-11-21 01:02:51 -0500

    updated seeder with playlists

--- backend/database/seeders/DatabaseSeeder.php
+++ backend/database/seeders/DatabaseSeeder.php
@@ -6,6 +6,7 @@ use App\Models\User;
 use App\Models\Tag;
 use App\Models\Video;
 use App\Models\Comment;
+use App\Models\Playlist;
 use App\Models\UserStrike;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -17,17 +18,26 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // creating initial tags, users, videos, and comments
+        // creating initial tags, users, and videos
         $tags = Tag::factory()->count(8)->create();
         $users = User::factory()
             ->count(3)
             ->has(
-                Video::factory()->count(3)->has(
-                    Comment::factory()->count(3)
-                )
+                Video::factory()->count(3)
             )
+            ->has(Playlist::factory())
             ->unverified()
             ->create();
+
+        // create comments for each video
+        foreach(Video::all() as $vid) {
+            $commenter = User::factory()->unverified()->create();
+            $comments = Comment::factory()
+                ->count(3)
+                ->for($vid)
+                ->for($commenter)
+                ->create();
+        }
 
         // attach tags to users to represent user interests
         foreach ($users as $user) {
@@ -36,7 +46,7 @@ class DatabaseSeeder extends Seeder
             );
         }
 
-        // Set comment replies to other comments within the last video
+        // set comment replies to other comments within the last video
         $video = Video::all()->last();
         $comments = Comment::where('video_id', $video->id)->get();
         $comments->get(1)->parentComment()->associate($comments->get(0));
@@ -52,11 +62,22 @@ class DatabaseSeeder extends Seeder
         $followed = User::factory()->unverified()->create();
         $followed->usersFollowedBy()->attach($users->last()->id, ['followed_at' => now()]);
 
-        $userWithStrike = User::factory()
+        // create a user with a strike
+        User::factory()
             ->has(
                 UserStrike::factory()
             )
             ->unverified()
             ->create();
+
+        // put videos in a playlist
+        $playlist = Playlist::all()->first();
+        $videosForPlaylist = Video::all()->take(4);
+        foreach ($videosForPlaylist as $key=>$vid) {
+            $vid->playlists()->attach($playlist->id, [
+                'added_at' => now(),
+                'order' => $key,
+            ]);
+        }
     }
 }
