f89284a7f1b053747c5eacd6d87a362cc9bcea9e
Luke <luke7ajb@gmail.com>
2024-11-20 23:18:17 -0500

    Added videos and comments to database seeder

--- backend/database/seeders/DatabaseSeeder.php
+++ backend/database/seeders/DatabaseSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 use App\Models\User;
 use App\Models\Tag;
+use App\Models\Video;
+use App\Models\Comment;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -14,9 +16,17 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // creating initial tags and users
+        // creating initial tags, users, videos, and comments
         $tags = Tag::factory()->count(8)->create();
-        $users = User::factory()->count(3)->unverified()->create();
+        $users = User::factory()
+            ->count(3)
+            ->has(
+                Video::factory()->count(3)->has(
+                    Comment::factory()->count(3)
+                )
+            )
+            ->unverified()
+            ->create();
 
         // attach tags to users to represent user interests
         foreach ($users as $user) {
@@ -24,6 +34,13 @@ class DatabaseSeeder extends Seeder
                 $tags->random(rand(2, 4))->pluck('id')->toArray()
             );
         }
+
+        // Set comment replies to other comments within the last video
+        $video = Video::all()->last();
+        $comments = Comment::where('video_id', $video->id)->get();
+        $comments->get(1)->parentComment()->associate($comments->get(0));
+        $comments->get(1)->save();
+        $comments->get(1)->replies()->save($comments->get(2));
 
         // create a new follower who follows the first user
         $follower = User::factory()->unverified()->create();
@@ -33,5 +50,7 @@ class DatabaseSeeder extends Seeder
         // users collection
         $followed = User::factory()->unverified()->create();
         $followed->usersFollowedBy()->attach($users->last()->id, ['followed_at' => now()]);
+
+
     }
 }
