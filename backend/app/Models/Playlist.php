cf4b0070df1b36f3cff36da5264b609befea0c9e
Luke <luke7ajb@gmail.com>
2024-11-21 00:16:28 -0500

    Created playlist factory

--- backend/app/Models/Playlist.php
+++ backend/app/Models/Playlist.php
@@ -3,11 +3,15 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Playlist extends Model
 {
+    use HasFactory, HasUuids;
+
     /**
      * The attributes that are mass assignable.
      *
