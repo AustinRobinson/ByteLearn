e390cb2c7b0af1746c23dc92f2784dbbb05e02ba
Luke <luke7ajb@gmail.com>
2024-11-20 21:03:19 -0500

    Added user_follows relationships between users in the database seeder

--- backend/database/seeders/DatabaseSeeder.php
+++ backend/database/seeders/DatabaseSeeder.php
@@ -14,13 +14,24 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+        // creating initial tags and users
         $tags = Tag::factory()->count(8)->create();
         $users = User::factory()->count(3)->unverified()->create();
 
+        // attach tags to users to represent user interests
         foreach ($users as $user) {
             $user->tags()->attach(
                 $tags->random(rand(2, 4))->pluck('id')->toArray()
             );
         }
+
+        // create a new follower who follows the first user
+        $follower = User::factory()->unverified()->create();
+        $follower->usersFollows()->attach($users[0]->id, ['followed_at' => now()]);
+
+        // create a new creator who is followed by the third user in the above
+        // users collection
+        $followed = User::factory()->unverified()->create();
+        $followed->usersFollowedBy()->attach($users->last()->id, ['followed_at' => now()]);
     }
 }
