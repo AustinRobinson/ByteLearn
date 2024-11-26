db1ac5302758318308e597f120b0eefaf0108ef8
austinrobinson <amrobinson28@icloud.com>
2024-11-26 10:03:24 -0500

    move pages into pages folder for cleaner structure

---
+++ frontend/src/app/pages/login/login.component.ts
@@ -0,0 +1,74 @@
+import { Component } from '@angular/core';
+import { HeaderComponent } from '../../components/header/header.component';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { ApiErrorResponse, AuthService, LoginFormData } from '../../services/auth/auth.service';
+import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [HeaderComponent, ReactiveFormsModule, CommonModule],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css'
+})
+export class LoginComponent {
+
+  // all validation is handled server-side
+  loginForm = new FormGroup({
+    email: new FormControl(''),
+    password: new FormControl(''),
+  });
+
+  constructor(private router: Router, private authService: AuthService) { }
+
+  /**
+   * Get the email form control.
+   */
+  public get email() {
+    return this.loginForm.get('email');
+  }
+
+  /**
+   * Get the password form control.
+   */
+  public get password() {
+    return this.loginForm.get('password');
+  }
+
+  onSubmit() {
+    // get form values
+    const formValues = this.loginForm.value;
+
+    // authenticate using auth service
+    this.authService.login(formValues as LoginFormData).subscribe({
+      next: (response) => {
+        this.router.navigateByUrl('/video-feed');
+      },
+      error: (error: ApiErrorResponse) => {
+        console.error('Login failed:', error.error.message);
+
+        // check if email or password is incorrect
+        if (error?.status === 401) {
+          this.loginForm.setErrors({ serverError: 'Email or password is incorrect' });
+          return;
+        }
+
+        const fieldErrors = error?.error?.errors;
+
+        // check if the response has field errors
+        if (fieldErrors) {
+          // for each field with an error
+          Object.keys(fieldErrors).forEach((field) => {
+            const control = this.loginForm.get(field);
+            if (control) {
+              // display the first error message for the field
+              control.setErrors({ serverError: fieldErrors[field][0] });
+            }
+          });
+        }
+      }
+    });
+  }
+
+}
---
+++ frontend/src/app/services/auth/auth.service.ts
@@ -0,0 +1,79 @@
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable, tap, throwError } from 'rxjs';
+import { environment } from '../../../environments/environment';
+
+/**
+ * Signup form data interface.
+ */
+export interface SignupFormData {
+  first_name: string;
+  last_name: string;
+  username: string;
+  email: string;
+  password: string;
+  password_confirmation: string;
+};
+
+/**
+ * Login form data interface.
+ */
+export interface LoginFormData {
+  email: string;
+  password: string;
+};
+
+/**
+ * Login response interface.
+ */
+export interface LoginResponse {
+  accessToken: string;
+}
+
+
+/**
+ * API error response interface.
+ */
+export interface ApiErrorResponse extends HttpErrorResponse {
+  error: {
+    // general message about the error
+    message: string;
+    errors: {
+      // key-value pairs where the key is the field name and the value is an array of error messages
+      [key: string]: string[];
+    };
+  }
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  private accessToken: string | null = null;
+
+  constructor(private http: HttpClient) {
+
+  }
+
+  /**
+   * Make a signup request to the API.
+   *
+   * @returns a cold Observable with the response
+   */
+  public signup(signupData: SignupFormData): Observable<any> {
+    return this.http.post(`${environment.apiBaseUrl}/register`, signupData);
+  }
+
+  /**
+   * Make a login request to the API.
+   *
+   * @returns a cold Observable with the response
+   */
+  public login(formData: LoginFormData): Observable<LoginResponse> {
+    return this.http.post<LoginResponse>(`${environment.apiBaseUrl}/login`, formData).pipe(
+      tap((response) => {
+        this.accessToken = response.accessToken;
+      }),
+    );
+  }
+}
