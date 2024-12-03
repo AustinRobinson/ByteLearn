8f7ecb68e88dcd85537566f07c6a5f2c38ba97e0
Luke Bushur <97570706+lukebushur@users.noreply.github.com>
2024-12-03 15:51:56 -0500

    Added comments to component and service (#24)
    * Added comments to component and service
    
    * moved videos service to inside app

--- frontend/src/app/pages/upload-video/upload-video.component.ts
+++ frontend/src/app/pages/upload-video/upload-video.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from "../../components/header/header.component";
 import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
-import { uploadFormData, VideoService } from '../../../services/video/video.service';
+import { uploadFormData, VideoService } from '../../services/video/video.service';
 
 @Component({
   selector: 'app-upload-video',
@@ -23,30 +23,38 @@ export class UploadVideoComponent {
     description: new FormControl(''),
   });
 
+  // construct the component with the injected video service
   public constructor(private videoService: VideoService) { }
 
+  // get the form group's title control
   public get title() {
     return this.uploadForm.get('title');
   }
 
+  // get the form group's description control
   public get description() {
     return this.uploadForm.get('description');
   }
 
+  // prevent default behavior upon being dragged over
   public onDragOver(event: Event): void {
     event.preventDefault();
   }
 
+  // increment the counter upon drag start
   public onDragEnter(event: Event): void {
     event.preventDefault();
 
     this.dragCounter++;
   }
 
+  // decrement the counter upon drag leave
   public onDragLeave(event: Event): void {
     this.dragCounter--;
   }
 
+  // upon dropping, get the file that was dropped and call onFileChange with the
+  // dropped file
   public onDropSuccess(event: any): void {
     event.preventDefault();
     this.dragCounter = 0;
@@ -59,6 +67,8 @@ export class UploadVideoComponent {
     this.onFileChange(event.dataTransfer.files);
   }
 
+  // upon selecting a file, get the file and call onFileChange with the
+  // selected file
   public onFileSelected(event: Event): void {
     const input: HTMLInputElement = event.currentTarget as HTMLInputElement;
     const files: FileList | null = input.files;
@@ -69,6 +79,8 @@ export class UploadVideoComponent {
     this.onFileChange(files);
   }
 
+  // Change the video to the given file. Read data from the file and update
+  // videoUrl.
   private onFileChange(files: FileList): void {
     this.video = files.item(0)!;
 
@@ -85,6 +97,8 @@ export class UploadVideoComponent {
     reader.readAsDataURL(this.video);
   }
 
+  // upon form submission, make a request to the back-end API with the video and
+  // its details (title and description)
   public onSubmit(): void {
     const uploadData: uploadFormData = {
       video: this.video!,
--- frontend/src/app/services/video/video.service.ts
+++ frontend/src/app/services/video/video.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { environment } from '../../environments/environment';
+import { environment } from '../../../environments/environment';
 
 export interface uploadFormData {
   video: File;
@@ -13,8 +13,12 @@ export interface uploadFormData {
 })
 export class VideoService {
 
+  // construct the video service with the injected HTTP client
   constructor(private http: HttpClient) { }
 
+  // Make a request to the back-end API to upload the video. Puts the video
+  // data (video file, title, description) in a FormData object and sends the
+  // data as multipart form data.
   public uploadVideo(data: uploadFormData) {
     const formData = new FormData();
     formData.append('video', data.video, data.video.name);
