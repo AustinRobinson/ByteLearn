2a522ae7dc3fb63e28ad8f46edfb7fc878e60ac2
austinrobinson <amrobinson28@icloud.com>
2024-12-05 13:31:52 -0500

    only show sign up if not authenticated

--- frontend/src/app/pages/home/home.component.ts
+++ frontend/src/app/pages/home/home.component.ts
@@ -1,14 +1,18 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../../components/header/header.component';
 import { RouterLink } from '@angular/router';
+import { AuthService } from '../../services/auth/auth.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeaderComponent, RouterLink],
+  imports: [CommonModule, HeaderComponent, RouterLink],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+
+  constructor(public authService: AuthService) { }
 
 }
