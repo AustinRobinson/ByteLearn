3a636b38361e9768f85bea177d8435603547f54d
Luke <luke7ajb@gmail.com>
2024-12-05 02:06:16 -0500

    Added methods to video service to perform video operations

--- frontend/src/app/services/video/video.service.ts
+++ frontend/src/app/services/video/video.service.ts
@@ -1,11 +1,38 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from '../../../environments/environment';
+import { Observable, of } from 'rxjs';
 
 export interface uploadFormData {
   video: File;
   title: string;
   description: string;
+}
+
+export interface Tag {
+  id: string,
+  tag: string,
+}
+
+export interface VideoDetails {
+  id: string;
+  title: string;
+  description: string;
+  created_at: string;
+  user: {
+    id: string,
+    username: string,
+  };
+  tags: Tag[];
+  has_watched: boolean;
+  is_liked: boolean;
+  comment_count: number;
+  like_count: number;
+  matches_interests: boolean;
+}
+
+export interface VideoFeed {
+  id: string;
 }
 
 @Injectable({
@@ -32,5 +59,62 @@ export class VideoService {
     return this.http.post(`${environment.apiBaseUrl}/videos/upload`, formData, {
       headers: headers,
     });
+  }
+
+  public toggleVideoLike(videoId: string): Observable<any> {
+    const data = {
+      video_id: videoId,
+    };
+
+    // return this.http.post(`${environment.apiBaseUrl}/videos/likes/toggle`, data);
+    return of(data);
+  }
+
+  public getVideo(videoId: string): Observable<VideoDetails> {
+    // return this.http.get<VideoDetails>(`${environment.apiBaseUrl}/videos/${videoId}`);
+    const video: VideoDetails = {
+      id: "12345678-abcd-1234-abcd-1234567890ab",
+      title: "Learn Python in 60 Seconds",
+      description: "Quick Python tutorial for beginners",
+      created_at: "2024-12-03T21:09:47.103Z",
+      user: {
+        id: "12345678-abcd-1234-abcd-1234567890ab",
+        username: "techteacher"
+      },
+      tags: [
+        {
+          id: "12345678-abcd-1234-abcd-1234567890ab",
+          tag: "python"
+        },
+        {
+          id: "12345678-abcd-1234-abcd-1234567890ab",
+          tag: "technology"
+        }
+      ],
+      is_liked: false,
+      has_watched: false,
+      comment_count: 2,
+      like_count: 56,
+      matches_interests: true,
+    };
+
+    return of(video);
+  }
+
+  public getVideoUrl(videoId: string): Observable<string> {
+    return of('./smile.mp4');
+  }
+
+  public videoFeed(offset: number, limit: number): Observable<VideoFeed[]> {
+    // return this.http.get<VideoDetails>(`${environment.apiBaseUrl}/videos/feed?offset=${offset}&limit=${limit}`);
+    const feed: VideoFeed[] = [];
+
+    for (let i = offset * limit; i < (offset + 1) * limit; ++i) {
+      feed.push({
+        id: `${i}`,
+      });
+    }
+
+    return of(feed);
   }
 }
