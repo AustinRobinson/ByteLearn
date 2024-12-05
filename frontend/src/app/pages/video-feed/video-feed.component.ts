352e317b3607985d74f2863aed63cb09c4ea2b4b
Luke <luke7ajb@gmail.com>
2024-12-05 02:07:48 -0500

    updated video feed to use video player component, added functionality for previous and next buttons

--- frontend/src/app/pages/video-feed/video-feed.component.ts
+++ frontend/src/app/pages/video-feed/video-feed.component.ts
@@ -1,155 +1,83 @@
-import { Component, OnInit } from '@angular/core';
-import { VideoService } from '../../services/video/video.service';
+import { Component, computed, OnInit, Signal, signal, untracked } from '@angular/core';
+import { VideoFeed, VideoService } from '../../services/video/video.service';
 import { HeaderComponent } from "../../components/header/header.component";
-import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
-import { DatePipe } from '@angular/common';
+import { VideoPlayerComponent } from "../../components/video-player/video-player.component";
+
+export const FEED_LIMIT: number = 20;
 
 @Component({
   selector: 'app-video-feed',
   standalone: true,
-  imports: [HeaderComponent, ReactiveFormsModule, DatePipe],
+  imports: [
+    HeaderComponent,
+    VideoPlayerComponent,
+  ],
   templateUrl: './video-feed.component.html',
   styleUrl: './video-feed.component.css'
 })
 export class VideoFeedComponent implements OnInit {
+  public offset = signal(0);
+  public videoIndex = signal(0);
 
-  public isDetailsOpen = false;
-  public details = {
-    "id": "12345678-abcd-1234-abcd-1234567890ab",
-    "title": "Learn Python in 60 Seconds",
-    "description": "Quick Python tutorial for beginners",
-    "video_url": "./smile.mp4",
-    "created_at": "2024-12-03T21:09:47.103Z",
-    "user": {
-      "id": "12345678-abcd-1234-abcd-1234567890ab",
-      "username": "techteacher"
-    },
-    "tags": [
-      {
-        "id": "12345678-abcd-1234-abcd-1234567890ab",
-        "tag": "python"
-      },
-      {
-        "id": "12345678-abcd-1234-abcd-1234567890ab",
-        "tag": "technology"
-      }
-    ],
-    "is_liked": false,
-    "has_watched": false,
-    "comment_count": 2,
-    "like_count": 56,
-    "matches_interests": true
-  }
-
-  public isVideoLiked = false;
-
-  public isCommentsOpen = false;
-  public comments = [
-    {
-      username: '@JaneDoe',
-      comment: 'This is an amazing post! He really likes it! Also, this is a really long comment, so if you want to see the like button, it\'s going to be a long ways down! 😊',
-      likes: 12,
-      isLiked: false,
-    },
-    {
-      username: '@JohnSmith',
-      comment: 'Thanks for sharing!',
-      likes: 8,
-      isLiked: false,
-    },
-    {
-      username: '@JaneDoe',
-      comment: 'This is an amazing post! He really likes it! Also, this is a really long comment, so if you want to see the like button, it\'s going to be a long ways down! 😊',
-      likes: 12,
-      isLiked: false,
-    },
-    {
-      username: '@JaneDoe',
-      comment: 'This is an amazing post! He really likes it! Also, this is a really long comment, so if you want to see the like button, it\'s going to be a long ways down! 😊',
-      likes: 12,
-      isLiked: false,
-    },
-    {
-      username: '@JaneDoe',
-      comment: 'This is an amazing post! He really likes it! Also, this is a really long comment, so if you want to see the like button, it\'s going to be a long ways down! 😊',
-      likes: 12,
-      isLiked: false,
-    },
-    {
-      username: '@JaneDoe',
-      comment: 'This is an amazing post! He really likes it! Also, this is a really long comment, so if you want to see the like button, it\'s going to be a long ways down! 😊',
-      likes: 12,
-      isLiked: false,
-    },
-    {
-      username: '@JaneDoe',
-      comment: 'This is an amazing post! He really likes it! Also, this is a really long comment, so if you want to see the like button, it\'s going to be a long ways down! 😊',
-      likes: 12,
-      isLiked: false,
-    },
-    {
-      username: '@JaneDoe',
-      comment: 'This is an amazing post! He really likes it! Also, this is a really long comment, so if you want to see the like button, it\'s going to be a long ways down! 😊',
-      likes: 12,
-      isLiked: false,
-    },
-    {
-      username: '@JaneDoe',
-      comment: 'This is an amazing post! He really likes it! Also, this is a really long comment, so if you want to see the like button, it\'s going to be a long ways down! 😊',
-      likes: 12,
-      isLiked: false,
-    },
-  ];
-
-  public commentForm = new FormGroup({
-    comment: new FormControl(''),
+  public currentFeed = signal<VideoFeed[]>([]);
+  public videoId: Signal<string> = computed(() => {
+    console.log('changed to ' + this.currentFeed()[this.videoIndex()].id);
+    return this.currentFeed()[this.videoIndex()].id
   });
 
-  public constructor(private videoService: VideoService) { }
+  public isPreviousDisabled: Signal<boolean> = computed(() => {
+    return this.videoIndex() === 0 && this.offset() === 0
+  });
+  public isNextDisabled = signal(false);
+
+  public constructor(
+    private videoService: VideoService
+  ) {}
 
   public ngOnInit(): void {
+    this.updateFeed();
+  }
 
+  private updateFeed(): void {
+    this.videoService.videoFeed(this.offset(), FEED_LIMIT).subscribe({
+      next: (feed: VideoFeed[]) => {
+        console.log('updated feed');
+        this.currentFeed.set(feed);
+      },
+      error: (error: any) => {
+        console.log('Error fetching feed', error);
+      }
+    });
   }
 
   public previousVideo(): void {
     console.log('Clicked Previous');
+    if (this.isPreviousDisabled()) {
+      return;
+    }
+
+    untracked(() => {
+      this.videoIndex.update(value => value !== 0 ? value - 1 : FEED_LIMIT - 1);
+
+      if (this.videoIndex() === FEED_LIMIT - 1) {
+        this.offset.update(value => value - 1);
+        this.updateFeed();
+      }
+    });
   }
 
   public nextVideo(): void {
     console.log('Clicked Next');
-  }
-
-  public toggleDetails(): void {
-    this.isDetailsOpen = !this.isDetailsOpen;
-  }
-
-  public toggleVideoLike(): void {
-    if (!this.isVideoLiked) {
-      ++this.details.like_count;
-    } else {
-      --this.details.like_count;
+    if (this.isNextDisabled()) {
+      return;
     }
-    this.isVideoLiked = !this.isVideoLiked;
-  }
 
-  public toggleComments(): void {
-    this.isCommentsOpen = !this.isCommentsOpen;
-  }
-
-  public clearComment(): void {
-    this.commentForm.reset();
-  }
-
-  public onComment(): void {
-    console.log(this.commentForm.get('comment')?.value);
-  }
-
-  public toggleLike(comment: any): void {
-    if (!comment.isLiked) {
-      ++comment.likes;
-    } else {
-      --comment.likes;
-    }
-    comment.isLiked = !comment.isLiked;
+    untracked(() => {
+      this.videoIndex.update(value => (value + 1) % FEED_LIMIT);
+      if (this.videoIndex() === 0) {
+        this.offset.update(value => value + 1);
+        this.updateFeed();
+      }
+    });
   }
 }
