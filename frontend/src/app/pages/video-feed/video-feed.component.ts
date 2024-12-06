51ba8e9b10b80368c19df335d8f7c99b4b6922a7
Luke <luke7ajb@gmail.com>
2024-12-06 17:51:46 -0500

    added documentation, feed history is now kept track of

--- frontend/src/app/pages/video-feed/video-feed.component.ts
+++ frontend/src/app/pages/video-feed/video-feed.component.ts
@@ -1,10 +1,11 @@
-import { Component, computed, OnInit, Signal, signal, untracked } from '@angular/core';
+import { Component, computed, OnInit, Signal, signal } from '@angular/core';
 import { VideoFeed, VideoService } from '../../services/video/video.service';
 import { HeaderComponent } from "../../components/header/header.component";
 import { VideoPlayerComponent } from "../../components/video-player/video-player.component";
 
 export const FEED_LIMIT: number = 20;
 
+// Page for viewing the user's video feed.
 @Component({
   selector: 'app-video-feed',
   standalone: true,
@@ -19,31 +20,66 @@ export class VideoFeedComponent implements OnInit {
   public offset = signal(0);
   public videoIndex = signal(0);
 
-  public currentFeed = signal<VideoFeed[]>([]);
+  // signal holding current video feed
+  public videoFeed = signal<VideoFeed[]>([]);
+
+  // ID of the current video based on video feed and index
   public videoId: Signal<string> = computed(() => {
-    return this.currentFeed()[this.videoIndex()].id;
-  });
-  public videoS3Key: Signal<string> = computed(() => {
-    return this.currentFeed()[this.videoIndex()].s3_key;
+    let indexInUse;
+    if (this.videoIndex() === 0) {
+      indexInUse = 0;
+    } else {
+      indexInUse = this.videoIndex() >= this.videoFeed().length
+        ? this.videoIndex() - 1
+        : this.videoIndex();
+    }
+
+    return this.videoFeed()[indexInUse].id;
   });
 
-  public isPreviousDisabled: Signal<boolean> = computed(() => {
-    return this.videoIndex() === 0 && this.offset() === 0
+  // s3 key of the current video based on video feed and index
+  public videoS3Key: Signal<string> = computed(() => {
+    let indexInUse;
+    if (this.videoIndex() === 0) {
+      indexInUse = 0;
+    } else {
+      indexInUse = this.videoIndex() >= this.videoFeed().length
+        ? this.videoIndex() - 1
+        : this.videoIndex();
+    }
+
+    return this.videoFeed()[indexInUse].s3_key;
   });
+
+  // whether the previous video button is disabled
+  public isPreviousDisabled: Signal<boolean> = computed(() => {
+    return this.videoIndex() <= 0;
+  });
+  // whether the next video button is disabled (currently, there is no
+  // reason for it to be disabled)
   public isNextDisabled = signal(false);
 
   public constructor(
     private videoService: VideoService
   ) {}
 
+  // Get the first feed page on page load
   public ngOnInit(): void {
-    this.updateFeed();
+    this.addNextFeed();
   }
 
-  private updateFeed(): void {
+  // update the current video feed page
+  private addNextFeed(): void {
+    console.log('called');
+
     this.videoService.videoFeed(this.offset(), FEED_LIMIT).subscribe({
       next: (feed: VideoFeed[]) => {
-        this.currentFeed.set(feed);
+        this.videoFeed.update(currentFeed => {
+          return [
+            ...currentFeed,
+            ...feed,
+          ];
+        });
       },
       error: (error: any) => {
         console.log('Error fetching feed', error);
@@ -51,32 +87,29 @@ export class VideoFeedComponent implements OnInit {
     });
   }
 
+  // Go to previous video in feed if possible
   public previousVideo(): void {
     if (this.isPreviousDisabled()) {
       return;
     }
 
-    untracked(() => {
-      this.videoIndex.update(value => value !== 0 ? value - 1 : FEED_LIMIT - 1);
-
-      if (this.videoIndex() === FEED_LIMIT - 1) {
-        this.offset.update(value => value - 1);
-        this.updateFeed();
-      }
-    });
+    this.videoIndex.update(value => value - 1);
+    console.log('index prev: ', this.videoIndex());
+    console.log(this.videoFeed());
   }
 
+  // Go to next video in feed
   public nextVideo(): void {
     if (this.isNextDisabled()) {
       return;
     }
 
-    untracked(() => {
-      this.videoIndex.update(value => (value + 1) % FEED_LIMIT);
-      if (this.videoIndex() === 0) {
-        this.offset.update(value => value + 1);
-        this.updateFeed();
-      }
-    });
+    this.videoIndex.update(value => value + 1);
+    if (this.videoIndex() >= this.videoFeed().length) {
+      this.offset.update(value => value + 1);
+      this.addNextFeed();
+    }
+    console.log('index next: ', this.videoIndex());
+    console.log(this.videoFeed());
   }
 }
