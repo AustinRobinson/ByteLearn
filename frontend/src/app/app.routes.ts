8785e6340bacc572f23637fbfb576f8af8e65958
Luke <luke7ajb@gmail.com>
2024-12-06 14:16:24 -0500

    Added view single video route

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -5,6 +5,7 @@ import { SignupComponent } from './pages/signup/signup.component';
 import { UploadVideoComponent } from './pages/upload-video/upload-video.component';
 import { authGuard } from './guards/auth/auth.guard';
 import { VideoFeedComponent } from './pages/video-feed/video-feed.component';
+import { ViewVideoComponent } from './pages/view-video/view-video.component';
 
 export const routes: Routes = [
   // {
@@ -23,15 +24,21 @@ export const routes: Routes = [
   {
     path: 'upload-video',
     component: UploadVideoComponent,
-    canActivate: [authGuard]
-  },
-  {
-    path: '',
-    component: HomeComponent,
+    canActivate: [authGuard],
   },
   {
     path: 'video-feed',
     component: VideoFeedComponent,
+    canActivate: [authGuard],
+  },
+  {
+    path: 'video/:id',
+    component: ViewVideoComponent,
+    canActivate: [authGuard],
+  },
+  {
+    path: '',
+    component: HomeComponent,
   },
   {
     path: '**',
